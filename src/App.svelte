a42a2e54977d40817d0bb3aaa5408ada5f7839df
Sascha Aeppli <sascha.aeppli@liip.ch>
2021-02-23 19:59:50 +0100

    update preprocess

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,8 @@
 <h1 class="bg-purple-900 text-white p-3">Svelte, Windicss & TypeScript Template</h1>
+<h2 class="mt-2">
+    this is a subheader
+</h2>
+<div>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe voluptatum nihil necessitatibus quibusdam repellat et illum fugiat earum fugit nobis. Vitae assumenda cumque nihil tempora expedita, at sunt ipsam inventore.
+</div>
+<p class="bg-pink-100">this is super cool</p>
